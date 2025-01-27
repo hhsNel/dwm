231341072b0ada81890a8eefe69e76a19cd24d66
hhsNel <hihijsonel@gmail.com>
2025-01-27 23:32:37 +0100

    background color fix

--- config.h
+++ config.h
@@ -24,9 +24,9 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
-	/*               fg             bg         border   */
-	[SchemeNorm] = { col_darkgy,    col_black, col_darkgy     },
-	[SchemeSel]  = { col_darkgreen, col_black, col_darkyellow },
+	/*               fg             bg              border   */
+	[SchemeNorm] = { col_darkgy,    col_black,      col_darkgy     },
+	[SchemeSel]  = { col_darkgreen, col_darkyellow, col_darkyellow },
 };
 
 /* tagging */
