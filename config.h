4d9ef9f9a0327f466c7f28615c92b06ce6cf899f
hhsNel <hihijsonel@gmail.com>
2025-02-12 23:55:16 +0100

    dmenu n cols

--- config.h
+++ config.h
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", , "-sb", col_darkyellow, "-sf", col_darkgreen, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_darkgy, "-sb", col_darkyellow, "-sf", col_darkgreen, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "shiftview.c"
